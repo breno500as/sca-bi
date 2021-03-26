33d52ec1fc6ff5b412479924b5a7af3c60d4d05a
breno500as <breno500as@github.com>
2021-03-26 00:31:36 -0300

    teste

--- src/main/java/com/puc/sca/bi/ScaBiApplication.java
+++ src/main/java/com/puc/sca/bi/ScaBiApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 
+ * @author breno
+ *
+ */
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ScaBiApplication {
