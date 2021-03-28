0138114f2c1d06c11f5c3f574ee8c4826a9cffcb
breno500as <breno500as@gmail.com>
2021-03-28 17:59:03 -0300

    erro github

--- src/main/java/com/puc/sca/bi/ScaBiApplication.java
+++ src/main/java/com/puc/sca/bi/ScaBiApplication.java
@@ -5,9 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
- * BI
+ * https://docs.github.com/pt/github/setting-up-and-managing-your-github-profile/why-are-my-contributions-not-showing-up-on-my-profile
  * 
- * @author breno.
+ * No meu caso o erro era no email, estava breno500as@github.com e o correto é breno500as@gmail.com
+ * descobri aqui: https://github.com/breno500as/kafka-library-consumer/commit/3f6bffc4bf589d1879370b4c9205ea11981925a7.patch
+ * pegando o status e corrigi com:  git config --global user.email breno500as@gmail.com
+ * 
+ * 
+ * 
+ * @author breno. 
  */
 
 @SpringBootApplication
